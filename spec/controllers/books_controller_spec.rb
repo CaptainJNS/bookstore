ee093339e1665e73209352d160e2fd7dea663f44
Jason <sereda9623@gmail.com>
2019-08-21 15:50:17 +0300

    Changed sqlite3 to postgresql. Writed basic rspecs

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-  let(:book) { FactoryBot.build_stubbed(:book) }
+  let(:book) { FactoryBot.create(:book) }
 
   describe 'GET #show' do
     it 'renders :show template' do
       get :show, params: { id: book.id }
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #index' do
+    it 'renders :index template' do
+      get :index
+      expect(response).to render_template :index
     end
   end
 end
