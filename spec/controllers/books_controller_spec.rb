c88582dadc79fcf2c47154cba481749863da39cb
Jason <sereda9623@gmail.com>
2019-08-23 20:23:22 +0300

    Add some spec for book controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-  let(:book) { FactoryBot.create(:book) }
-
   describe 'GET #show' do
+    let(:book) { FactoryBot.create(:book) }
+
     it 'renders :show template' do
       get :show, params: { id: book.id }
       expect(response).to render_template :show
@@ -13,6 +13,26 @@ RSpec.describe BooksController, type: :controller do
   describe 'GET #index' do
     it 'renders :index template' do
       get :index
+      expect(response).to render_template :index
+    end
+
+    it 'when ascending title' do
+      get :index, params: { sort_by: '1' }
+      expect(response).to render_template :index
+    end
+
+    it 'when descending title' do
+      get :index, params: { sort_by: '2' }
+      expect(response).to render_template :index
+    end
+
+    it 'when ascending price' do
+      get :index, params: { sort_by: '3' }
+      expect(response).to render_template :index
+    end
+
+    it 'when descending price' do
+      get :index, params: { sort_by: '4' }
       expect(response).to render_template :index
     end
   end
