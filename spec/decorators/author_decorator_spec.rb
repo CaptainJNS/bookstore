8e61741b2137b91d95bca2624eb61fd9656ac6eb
Jason <sereda9623@gmail.com>
2019-09-04 14:53:46 +0300

    Added specs for decorators

--- spec/decorators/author_decorator_spec.rb
+++ spec/decorators/author_decorator_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AuthorDecorator do
+  subject(:author) { FactoryBot.create(:author) }
+
+  it '#full_name' do
+    expect(author.decorate.full_name).to eq("#{author.first_name} #{author.last_name}")
+  end
 end
--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe BookDecorator do
+  subject(:book) { FactoryBot.create(:book) }
+
+  it '#authors_names' do
+    expect(book.decorate.authors_names).to eq(book.authors.map { |author| author.decorate.full_name }.join(', '))
+  end
+
+  it '#categories_names' do
+  expect(book.decorate.categories_names).to eq(book.categories.map{ |category| category.name }.join(', '))
+  end
 end
