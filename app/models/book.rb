a3209364cf596c36dfa4e859a0f9189a50857304
Jason <sereda9623@gmail.com>
2019-09-12 14:09:56 +0300

    Added a model BooksCategory

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
   has_many :authors_books, dependent: :destroy
   has_many :authors, through: :authors_books
-  has_and_belongs_to_many :categories
+
+  has_many :books_categories, dependent: :destroy
+  has_many :categories, through: :books_categories
+
   has_many_attached :images
 
   validates :title, :description, :price, :year, :quantity, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_080612) do
+ActiveRecord::Schema.define(version: 2019_09_12_105242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +64,13 @@ ActiveRecord::Schema.define(version: 2019_09_11_080612) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "books_categories", id: false, force: :cascade do |t|
-    t.bigint "book_id", null: false
-    t.bigint "category_id", null: false
+  create_table "books_categories", force: :cascade do |t|
+    t.bigint 'book_id'
+    t.bigint 'category_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_books_categories_on_category_id'
+    t.index ['book_id'], name: 'index_books_categories_on_book_id'
   end
 
   create_table "categories", force: :cascade do |t|
@@ -78,4 +82,6 @@ ActiveRecord::Schema.define(version: 2019_09_11_080612) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key 'authors_books', 'authors'
   add_foreign_key 'authors_books', 'books'
+  add_foreign_key 'books_categories', 'categories'
+  add_foreign_key 'books_categories', 'books'
 end
