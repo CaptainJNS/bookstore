1b8cabc1226e61f8d53a0e19d243d3adef39b5e5
Jason <sereda9623@gmail.com>
2019-09-25 15:59:55 +0300

    Heroku emails with SendGrid [4]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'captainjns-bookstore.heroku.com' } 
+  config.action_mailer.default_url_options = { host: 'captainjns-bookstore.herokuapp.com' } 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
