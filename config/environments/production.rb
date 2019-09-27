be10d8c13e8b587834d354271d7377fbc554c130
Jason <sereda9623@gmail.com>
2019-09-27 12:36:56 +0300

    AWS S3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
     }
 
+<<<<<<< HEAD
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
@@ -112,4 +113,15 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION')
     }
   }
+=======
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        s3_region: ENV.fetch('AWS_REGION'),
+      }
+    }
+>>>>>>> AWS S3 config
 end
