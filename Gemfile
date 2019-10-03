995776993fff4e23e44694eece58802c3799f763
Jason <sereda9623@gmail.com>
2019-10-03 12:07:37 +0300

    capybara config

--- Gemfile
+++ Gemfile
@@ -86,6 +86,7 @@ group :test do
   gem 'shoulda-matchers'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
+  gem 'capybara-selenium', '~> 0.0.6'
   gem 'selenium-webdriver'
   gem 'simplecov', '~> 0.16.1'
   # Easy installation and use of chromedriver to run system tests with Chrome
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,27 +1,18 @@
-# require 'webdrivers/chromedriver'
-# require 'webdrivers/geckodriver'
-# require 'webdrivers/iedriver'
-# require 'webdrivers/edgedriver'
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
 
-# Capybara.register_driver :selenium_chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
+    }
+  )
 
-# Capybara.javascript_driver = :selenium_chrome
-# require "selenium/webdriver"
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
-
-# Capybara.register_driver :headless_chrome do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: { args: %w(headless disable-gpu) }
-#   )
-
-#   Capybara::Selenium::Driver.new app,
-#     browser: :chrome,
-#     desired_capabilities: capabilities
-# end
-
-# Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
