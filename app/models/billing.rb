b1353a69fac448645611a3cf05d37fa3f9b7d014
Jason <sereda9623@gmail.com>
2019-10-09 18:39:09 +0300

    Settings, Billing validation

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,3 +1,17 @@
 class Billing < ApplicationRecord
   belongs_to :user
+
+  validates(
+    :first_name,
+    :last_name,
+    :address,
+    :city,
+    :zip,
+    :country,
+    :phone,
+    presence: false
+  )
+
+  validates_format_of :phone, with: /\A[+]?[\d]{10,13}\z/, allow_blank: true
+  validates :zip, numericality: { only_integer: true }, allow_blank: true
 end
