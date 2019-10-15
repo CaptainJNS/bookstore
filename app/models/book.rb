11ceb38d9f631932a5612193fc5b1cd8c345dec3
Jason <sereda9623@gmail.com>
2019-10-15 03:07:36 +0300

    Reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ApplicationRecord
   has_many :authors_books, dependent: :destroy
   has_many :authors, through: :authors_books
-  accepts_nested_attributes_for :authors
-
   has_many :reviews, dependent: :destroy
 
   has_many :books_categories, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
   root 'home#index'
 
   resources :books
