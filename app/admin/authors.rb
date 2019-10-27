c68e0c626e6241217049363a4eafcfe0132cbd80
Jason <sereda9623@gmail.com>
2019-10-27 14:56:10 +0200

    Rebasing

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Author do
-<<<<<<< HEAD
   permit_params :first_name, :last_name
 
   decorate_with AuthorDecorator
@@ -13,20 +12,4 @@ ActiveAdmin.register Author do
 
     actions
   end
-=======
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
->>>>>>> Created admin panel
 end
