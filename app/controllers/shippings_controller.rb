5aec7580227fbc8f6fd1db3aed38b9f421c8d613
Jason <sereda9623@gmail.com>
2019-10-30 14:31:22 +0200

    Deleted extra files

--- app/controllers/shippings_controller.rb
+++
@@ -1,7 +0,0 @@
-class ShippingsController < ApplicationController
-  include AddressesHelper
-
-  def update
-    redirect_after_update(:shipping) if current_user.shipping.update(address_params(:shipping))
-  end
-end
