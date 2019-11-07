d04c624d740a46247458a3d8b3db050538a09259
Jason <sereda9623@gmail.com>
2019-11-07 21:58:18 +0200

    Changed UpdateTotalPrice

--- app/services/update_total_price.rb
+++ app/services/update_total_price.rb
@@ -1,7 +1,23 @@
 class UpdateTotalPrice
-  def call(order, price, quantity, method)
+  attr_reader :order, :price, :quantity, :action
+
+  def initialize(order, order_item, action)
+    @order = order
+    @price = order_item.price
+    @quantity = order_item.quantity
+    @action = action
+  end
+
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def call
     difference = price * quantity - order.discount(price)
 
-    method == :remove ? order.decrement!(:total_price, difference) : order.increment!(:total_price, difference)
+    case action
+    when :remove then order.decrement!(:total_price, difference)
+    when :add then order.increment!(:total_price, difference)
+    end
   end
 end
