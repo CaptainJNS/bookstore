25b857faddf6ff1008771921cca87f73005feb1c
Jason <sereda9623@gmail.com>
2019-11-08 17:51:27 +0200

    Mailer spec

--- spec/mailers/order_confirmation_mailer_spec.rb
+++ spec/mailers/order_confirmation_mailer_spec.rb
@@ -1,5 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe OrderConfirmationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:order) { create(:order, user: create(:user)) }
+
+  describe 'complete  ' do
+    let(:mail) { described_class.with(user: order.user).order_confirmation }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq(I18n.t('mail.completed'))
+      expect(mail.to).to eq([order.user.email])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(I18n.t('mail.thanks'))
+    end
+  end
 end
