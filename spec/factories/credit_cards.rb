b1839e29b096e56daf891436e284503f9ec905a6
Jason <sereda9623@gmail.com>
2019-11-09 15:33:16 +0200

    Change exp_date generation for credit card

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     card_name { FFaker::Lorem.word }
     number { rand(10).to_s * 16 }
     cvv { rand(10).to_s * 3 }
-    expiration_date { "#{rand(10..12)}/#{rand(20..30)}" }
+    expiration_date { (Time.now + rand(1..10).year * rand(0.0..1)).strftime('%m/%y') }
   end
 end
