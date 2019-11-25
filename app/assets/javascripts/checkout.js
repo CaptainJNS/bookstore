f29abff711499c566150da42bfa4307d6dde5fdd
Jason <sereda9623@gmail.com>
2019-11-25 18:00:09 +0200

    Delivery price fixed

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -19,3 +19,15 @@ let hide_shipping = (chek_box) => {
   $("input[name='user[shipping_attributes][country]'").prop('required', !$(chek_box).is(':checked'))
   $("input[name='user[shipping_attributes][phone]'").prop('required', !$(chek_box).is(':checked'))
 }
+
+let changeTotal = (total, price, old_price) => {
+  let order_total_element = document.getElementById('order_total');
+  let currency = order_total_element.textContent.substring(0, 1);
+  if (old_price == null) { old_price = 0 }
+  order_total_element.textContent = currency + (total + price - old_price).toFixed(2);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  $(".deliveries > tr").first().click();
+  $(".radio-input").first().prop('checked', true);
+}, false);
--- app/interactors/add_delivery.rb
+++ app/interactors/add_delivery.rb
@@ -2,6 +2,8 @@ class AddDelivery
   include Interactor
 
   def call
+    UpdateTotalPrice.call(context.current_order, delivery: context.current_order.delivery, action: :minus) if context.current_order.delivery.present?
+
     delivery = Delivery.find_by(id: context.delivery_id)
 
     return context.fail! unless delivery
